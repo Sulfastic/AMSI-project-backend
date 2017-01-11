646e04a422c511e40548dbefc77ecdcdaba1dd30
Paweł Marciniec <pawel.marciniec.pm@gmail.com>
2017-01-11 21:05:32 +0100

    Add CORS needed by frontend client, part 2

--- src/main/java/eu/needtocode/controller/UserLoginController.java
+++ src/main/java/eu/needtocode/controller/UserLoginController.java
@@ -3,6 +3,7 @@ package eu.needtocode.controller;
 import eu.needtocode.model.LoginUserRQ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
+@CrossOrigin
 @RequestMapping("users/login")
 public class UserLoginController {
 
